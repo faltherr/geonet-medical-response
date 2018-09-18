d7d6506ca9c87d81cf88ba1dab713136a060c932
Kyle Barnett <kbarnett23@gmail.com>
2018-09-18 11:36:05 -0600

    first commit login

---
+++ src/components/landing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  
+  login = () => {
+    let auth0domain = `https://${process.env.REACT_APP_AUTH0_DOMAIN}`
+    let clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
+    let scope = encodeURIComponent('openid profile email')
+    let redirectUri = encodeURIComponent(`${window.location.origin}/auth/callback`)
+    let location = `${auth0domain}/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&response_type=code`
+
+    window.location = location
+  }
+  render() { 
+    return (
+      <div>
+        Landing
+      </div>
+    );
+  }
+}
+ 
+export default Landing;
