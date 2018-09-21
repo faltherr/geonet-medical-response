d42f07820d977047def5c31c16eaf1913f130d3e
Alexandria Hart <alexandriahart@moondog.local>
2018-09-21 09:51:13 -0600

    fixed error with componentdidupdate

--- db/get_healthworkers.sql
+++ db/get_healthworkers.sql
@@ -1,4 +1,3 @@
-SELECT healthworkers.*, outposts.*, patients.name as patient_name  FROM healthworkers
+SELECT healthworkers.*, outposts.location as outpost_location FROM healthworkers
 JOIN outposts on healthworkers.outpost_id = outposts.id
-JOIN patients on healthworkers.id = patients.healthworker_id
 ORDER BY healthworkers.id;
