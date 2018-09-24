85741317ac7499b97ffe37222c48f90e8132650e
Alexandria Hart <alexandriahart@moondog.local>
2018-09-24 12:44:13 -0600

    fix add and update function in backend for surveys

--- db/add_patient.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO patients(name, age, sex, location, phone, active)
-Values($1, $2, $3, $4, $5, $6)
-RETURNING id;
--- server/controllers/SurveyCtrl.js
+++ server/controllers/SurveyCtrl.js
@@ -1,27 +1,29 @@
-// module.exports = {
-//   getSurveys: (req, res) => {
-//     const db = req.app.get('db')
+module.exports = {
+  addSurvey: (req,res) => {
+    let db = req.app.get('db')
+    let {name, phone,location, latitude, longitude, age, famplan, hiv, parity, duedate, alert, completed} = req.body
 
-//     db.get_surveys()
-//     .then(results => {
-//       res.status(200).send(results)
-//     })
-//     .catch(err => {
-//       console.log(err)
-//       res.status(500).send('Something went wrong with getting surveys')
-//     })
-//   },
-//   getSurvey:(req,res) => {
-//     const db = req.app.get('db')
-//     const {id} = req.params
-
-//     db.get_survey(id)
-//     .then(results => {
-//       res.status(200).send(results)
-//     })
-//     .catch(err => {
-//       console.log(err)
-//       res.status(500).send('Something went wrong with getting survey by id')
-//     })
-//   }
-// }
+    db.add_survey([name, phone,location, latitude, longitude, age, famplan, hiv, parity, duedate, alert, completed])
+    .then(results => {
+      res.status(200).send(results)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send('Something went wrong with adding patient')
+    })
+  },
+  updateSurvey: (req, res) => {
+    let {id} = req.params
+    let {name, phone,location, latitude, longitude, age, famplan, hiv, parity, duedate, alert, completed} = req.body
+    let db = req.app.get('db')
+    
+    db.update_survey([id, name, phone,location, latitude, longitude, age, famplan, hiv, parity, duedate, alert, completed])
+    .then(results => {
+      res.status(200).send(results)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send('Something went wrong with updating patient')
+    })
+  }
+}
